0d99de3328a6fcd1da3f748769d2c9891ebadd0a
borjasolerr <borjasolrey@gmail.com>
2019-03-27 17:47:19 +0100

    Finished assignment

---
+++ data/seeds/01-cohorts.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex, Promise) {
+  return knex('cohort')
+    .del()
+    .then(function() {
+      return knex('cohort').insert([{ name: 'WEBEU1' }, { name: 'WEBEU2' }, { name: 'WEBEU3' }]);
+    });
+};
